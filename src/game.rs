511a0aba1510557880e26b397884041fcf1a716d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-13 05:03:43 -0500

    Move things around

--- src/game.rs
+++ src/game.rs
@@ -1,16 +1,11 @@
-use std::{time::{Duration, Instant}, os};
+use std::time::{Duration, Instant};
 use std::thread::sleep;
 
 pub struct Game {
   should_close: bool,
+  goal_delta: f64,
   delta: f64,
   old_time: Instant
-}
-
-impl Drop for Game {
-  fn drop(&mut self) {
-    println!("Minetest dropped!");
-  }
 }
 
 impl Game {
@@ -19,6 +14,8 @@ impl Game {
     println!("Minetest initialized!");
     Game {
       should_close: false,
+      // 60 FPS goal for the moment.
+      goal_delta: 1.0 / 60.0,
       delta: 0.0,
       old_time: Instant::now(),
     }
@@ -33,7 +30,9 @@ impl Game {
   pub fn main(&mut self) {
     // This is an extremely overkill way to get delta time.
 
-    sleep(Duration::new(1,0));
+    println!("{}", self.goal_delta);
+
+    sleep(Duration::new(0,(self.goal_delta * 1_000_000_000.0).floor() as u32));
 
     self.delta = self.old_time.elapsed().as_secs_f64();
 
@@ -41,5 +40,11 @@ impl Game {
 
 
     self.old_time = Instant::now();
+  }
+}
+
+impl Drop for Game {
+  fn drop(&mut self) {
+    println!("Minetest dropped!");
   }
 }
