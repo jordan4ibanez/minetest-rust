2aff4e3a1c07eb25ef771a586cc8e7c16d7a33d6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-13 06:38:11 -0500

    Time to rebuild this from the ground up

---
+++ README.md
@@ -0,0 +1,4 @@
+# minetest-rust
+### server first, client second.
+
+Rebuilding minetest from the ground up to not be scary to make basic PRs.
--- src/game.rs
+++ src/game.rs
@@ -1,11 +1,6 @@
-use std::time::{Duration, Instant};
-use std::thread::sleep;
-
 pub struct Game {
   should_close: bool,
   goal_delta: f64,
-  delta: f64,
-  old_time: Instant
 }
 
 impl Game {
@@ -16,8 +11,6 @@ impl Game {
       should_close: false,
       // 60 FPS goal for the moment.
       goal_delta: 1.0 / 60.0,
-      delta: 0.0,
-      old_time: Instant::now(),
     }
   }
 
@@ -27,19 +20,15 @@ impl Game {
     }
   }
 
+  pub fn busy_work(&mut self) {
+    for i in 0..1_000 {
+
+    }
+  }
+
   pub fn main(&mut self) {
-    // This is an extremely overkill way to get delta time.
-
-    println!("{}", self.goal_delta);
-
-    sleep(Duration::new(0,(self.goal_delta * 1_000_000_000.0).floor() as u32));
-
-    self.delta = self.old_time.elapsed().as_secs_f64();
-
-    println!("running | delta: {}", self.delta);
 
 
-    self.old_time = Instant::now();
   }
 }
 
