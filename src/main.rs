1c2f27cddd8fa6ebd48b10011d1d19a7cd0dcb6b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-13 04:22:34 -0500

    Keep struct on the stack
    We shall encapsulate the ARC && Box elements within the struct and have it work as a unit

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 mod game;
 
-use std::sync::Arc;
-
 use game::{*};
 
 fn main() {
-  // Look into different data types.
-  let mut minetest = Box::new(Game::new());
-  minetest.enter_main_loop();
+  Game::new().enter_main_loop()
 }
