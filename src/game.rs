df43f4e1e436964248d7b34a7cd1e724205645ff
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-14 02:30:01 -0500

    Re-engineer the logic of creation

--- src/game.rs
+++ src/game.rs
@@ -1,29 +1,63 @@
+mod client;
+mod server;
+
 use spin_sleep::LoopHelper;
+
+use self::{client::Client, server::Server};
 
 pub struct Game {
   should_close: bool,
   goal_fps: f64,
   goal_tps: f64,
+  server: Option<Server>,
+  client: Option<Client>,
   loop_helper: LoopHelper,
   delta: f64,
   current_fps: f64,
 }
 
 impl Game {
-  pub fn new() -> Self {
+  pub fn new(is_client: bool) -> Self {
     println!("Minetest initialized!");
-    Game {
+
+    // We could parse the player's name instead from a file, or a first time ask. This is mutable after all.
+    let client = match is_client {
+      true => Some(Client::new(String::from("singleplayer"))),
+      false => None,
+    };
+
+    // 60 FPS goal for the moment.
+    let goal_fps = 60.0;
+
+    // 20 Tick Per Second goal.
+    let goal_tps = 20.0;
+
+    // Can auto deploy server and treat this struct like a simplified dispatcher.
+    let (server, loop_helper_goal) = match is_client {
+      false => (Some(Server::new()), goal_tps),
+      true => (None, goal_fps),
+    };
+
+    let loop_helper = LoopHelper::builder()
+      .report_interval_s(1.0)
+      .build_with_target_rate(loop_helper_goal);
+
+    let mut game = Game {
       should_close: false,
-      // 60 FPS goal for the moment.
-      goal_fps: 60.0,
-      // 20 Tick Per Second goal.
-      goal_tps: 20.0,
-      loop_helper: LoopHelper::builder()
-        .report_interval_s(1.0)
-        .build_with_target_rate(20.0),
+
+      goal_fps,
+      goal_tps,
+
+      client,
+      server,
+
+      loop_helper,
+      
       delta: 0.0,
       current_fps: 0.0,
-    }
+    };
+
+    game.loop_helper = return game;
   }
 
   pub fn enter_main_loop(&mut self) {
