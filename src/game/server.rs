5e6efc7e7703835b237865aaffe9b0a423e4f79a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-15 02:50:59 -0500

    Update server.rs

--- src/game/server.rs
+++ src/game/server.rs
@@ -1,10 +1,11 @@
 pub struct Server {
   // needs some kind of lock, maybe
-  cool: bool,
 }
 
 impl Server {
   pub fn new() -> Self {
-    Server { cool: true }
+    Server {
+      // todo
+    }
   }
 }
