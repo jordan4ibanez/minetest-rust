3cc6cbb25ed70798e7945ebd4fe5bf0a30b32729
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-15 07:17:41 -0500

    Make raw output debugging an option

--- src/game/lua_engine.rs
+++ src/game/lua_engine.rs
@@ -1,33 +1,33 @@
-
 use std::fs::read_to_string;
 
 use mlua::Lua;
-
 
 ///
 /// LuaEngine encapsulates the Luau virtual machine.
 /// It is done this way so we can utilize Luau as
 /// elegantly as possible.
-/// 
+///
 pub struct LuaEngine {
-  lua: Lua
+  lua: Lua,
+  output_code_string: bool,
 }
 
 impl LuaEngine {
-
   pub fn new() -> Self {
     LuaEngine {
-      lua: Lua::new()
+      lua: Lua::new(),
+      output_code_string: false,
     }
   }
 
   ///
   /// Completely unfiltered and unsandboxed file compiler/runner.
-  /// 
+  ///
   pub fn run_file(&self, file_location: String) {
     let raw_code_string = read_to_string(file_location).unwrap();
-    println!("{}", raw_code_string);
+    if self.output_code_string {
+      println!("{}", raw_code_string);
+    }
     let _ = self.lua.load(raw_code_string).exec();
   }
-  
 }
