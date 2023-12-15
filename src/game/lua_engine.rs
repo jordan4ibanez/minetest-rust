b8e7ba5626bb771a5b8237da2edae7c18e822832
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-15 06:19:07 -0500

    Explain myself

--- src/game/lua_engine.rs
+++ src/game/lua_engine.rs
@@ -1,6 +1,12 @@
 
 use std::path::Path;
 
+
+///
+/// LuaEngine encapsulates the Luau virtual machine.
+/// It is done this way so we can utilize Luau as
+/// elegantly as possible.
+/// 
 pub struct LuaEngine {}
 
 impl LuaEngine {
