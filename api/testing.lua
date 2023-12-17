5504a7619ccc792b0d1ab208bdb00eff21b3d7aa
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-17 18:39:48 -0500

    Delete internal testing

--- api/testing.lua
+++
@@ -1,9 +0,0 @@
-local minetest = require("api/api")
-local vector = require("api/vector")
-
-local vec_test: vector.Vec2 = vector.vec2(1, 2)
-
-minetest.register_on_step(function(delta: number)
-  print("hello from LuauJIT: " .. tostring(delta))
-end)
-
