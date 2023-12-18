933fc99994cdf5f816d1f4ec34c17ab60db476c9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-17 19:09:07 -0500

    Create file_helpers.rs

---
+++ src/game/lua_engine/file_helpers.rs
@@ -0,0 +1,14 @@
+use std::{fs, path::Path};
+
+///
+/// This module is a thin wrapper around the built in rust components.
+/// It is done like this so we can have razor sharp components to bolt
+/// into different elements of the engine.
+/// 
+
+
+
+pub fn dir_exists(path: String) -> bool {
+  Path::new(&path).exists()
+}
+
