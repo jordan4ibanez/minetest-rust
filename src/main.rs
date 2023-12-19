ac19fc3c033153f8e14793d2fea81fe6ed064b4b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-18 23:51:40 -0500

    The entire game is now a smart pointer

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,12 @@ pub mod game;
 use game::*;
 
 fn main() {
-  Game::new(true).enter_main_loop()
+  // Game is one big ol' ARC smart pointer.
+  //* - (A)tomically
+  //* - (R)eference
+  //* - (C)ounted
+  // That's why this is written like this.
+  // The main loop is literally borrowing it for the
+  // lifetime of the game.
+  (*Game::new(true)).borrow_mut().enter_main_loop()
 }
