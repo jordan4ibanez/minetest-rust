ada6683f3c70061e00f524cc6a5c8c1ade088972
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-20 11:06:26 -0500

    Server now implements it's own LuaEngine

--- src/game/server.rs
+++ src/game/server.rs
@@ -1,12 +1,24 @@
-pub struct Server {
-  // needs some kind of lock, maybe
+use std::{rc::Rc, cell::RefCell, borrow::BorrowMut};
+
+use super::{lua_engine::LuaEngine, Game};
+
+pub struct Server<'server> {
+  lua_engine: Option<LuaEngine<'server>>,
+  game_pointer: Rc<RefCell<Game<'server>>>
 }
 
-impl Server {
-  pub fn new() -> Self {
-    Server {
-      // todo
-    }
+impl<'server> Server<'server> {
+  pub fn new(game_pointer: Rc<RefCell<Game<'server>>>) -> Self {
+    let mut new_server = Server {
+      lua_engine: None,
+      game_pointer: game_pointer.clone()
+    };
+
+    new_server.lua_engine = Some(LuaEngine::new(game_pointer.clone(), true));
+
+    new_server
+  }
+
   }
 
   pub fn on_tick(&mut self, delta: f64) {
