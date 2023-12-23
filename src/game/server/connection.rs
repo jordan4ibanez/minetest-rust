6d67ed82d14f8f99dc05d6e8a1dd208840172848
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-23 09:38:54 -0500

    fmt

--- src/game/server/connection.rs
+++ src/game/server/connection.rs
@@ -1,4 +1,3 @@
-
 pub struct Connection {
   address: String,
   port: i32,
@@ -8,7 +7,7 @@ impl Connection {
   pub fn new() -> Self {
     Connection {
       address: "127.0.0.1".to_string(),
-      port: 30_001
+      port: 30_001,
     }
   }
 }
