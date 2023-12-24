55012efb9fb721ec9b824afa9533a46717f15cfe
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-23 21:56:52 -0500

    Add a prototype to compose into a...
    ...parsable address

--- src/game/server/server_connection.rs
+++ src/game/server/server_connection.rs
@@ -10,4 +10,14 @@ impl ServerConnection {
       port: 30_001,
     }
   }
+
+  pub fn get_address(&self) -> String {
+    let mut final_address = self.address.clone();
+    final_address.push(':');
+    final_address.push_str(self.port.to_string().as_str());
+
+    println!("{}", final_address);
+
+    final_address
+  }
 }
