f611975ad59fe4d35b8f76d4bcd39f93fbee5cb9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-24 10:05:53 -0500

    Client can use CLI

--- src/game/client/client_connection.rs
+++ src/game/client/client_connection.rs
@@ -4,29 +4,28 @@ use super::Client;
 
 ///
 /// ClientConnection and Client can be considered 1 entity.
-/// 
+///
 /// This is why client_pointer is not an Option<>.
-/// 
+///
 pub struct ClientConnection<'client> {
   address: String,
   port: i32,
 
-  client_pointer: Rc<RefCell<Client<'client>>>
+  client_pointer: Rc<RefCell<Client<'client>>>,
 }
 
 impl<'client> ClientConnection<'client> {
-  pub fn new(client_pointer: Rc<RefCell<Client<'client>>>) -> Self {
+  pub fn new(client_pointer: Rc<RefCell<Client<'client>>>, address: String, port: i32) -> Self {
     let new_client_connection = ClientConnection {
-      address: "127.0.0.1".to_string(),
-      port: 30_001,
+      address,
+      port,
 
-      client_pointer
+      client_pointer,
     };
 
     new_client_connection
   }
 }
-
 
 impl<'client> Drop for ClientConnection<'client> {
   fn drop(&mut self) {
