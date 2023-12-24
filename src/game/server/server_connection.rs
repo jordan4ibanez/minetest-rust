a26c98165d6a42348f5df0438ea3c830b2083504
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-23 22:08:14 -0500

    Add setters for address & port

--- src/game/server/server_connection.rs
+++ src/game/server/server_connection.rs
@@ -11,12 +11,27 @@ impl ServerConnection {
     }
   }
 
+  ///
+  /// Change the address that the server connection will utilize.
+  /// 
+  pub fn set_address(&mut self, new_address: String) {
+    self.address = new_address;
+  }
+
+  ///
+  /// Change the port that the server connection will utilize.
+  /// 
+  pub fn set_port(&mut self, new_port: i32) {
+    self.port = new_port;
+  }
+
+  ///
+  /// Construct the address & port into a parsable string.
+  /// 
   pub fn get_address(&self) -> String {
     let mut final_address = self.address.clone();
     final_address.push(':');
     final_address.push_str(self.port.to_string().as_str());
-
-    println!("{}", final_address);
 
     final_address
   }
