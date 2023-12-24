2725aa039401220a016d2817d2167c3f6aa77115
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-24 10:25:16 -0500

    Fix a typo

--- src/game/server/server_connection.rs
+++ src/game/server/server_connection.rs
@@ -112,11 +112,11 @@ impl<'server> ServerConnection<'server> {
   ///
   fn initialize(&mut self) {
     let socket_address = self.get_socket().to_socket_addrs().unwrap().next().unwrap();
-    let transpor_protocol = Transport::Udp;
+    let transport_protocol = Transport::Udp;
 
     let (handler, listener) = node::split::<()>();
 
-    match handler.network().listen(transpor_protocol, socket_address) {
+    match handler.network().listen(transport_protocol, socket_address) {
       Ok((id, real_address)) => {
         println!(
           "minetest: connection created at id [{}], real address [{}]",
