bd314801c594d3c98fdeb30600959837fd545135
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-24 10:24:08 -0500

    These can be traits in the future

--- src/game/client/client_connection.rs
+++ src/game/client/client_connection.rs
@@ -16,14 +16,49 @@ pub struct ClientConnection<'client> {
 
 impl<'client> ClientConnection<'client> {
   pub fn new(client_pointer: Rc<RefCell<Client<'client>>>, address: String, port: i32) -> Self {
-    let new_client_connection = ClientConnection {
+    let mut new_client_connection = ClientConnection {
       address,
       port,
 
       client_pointer,
     };
 
+    new_client_connection.initialize();
+
     new_client_connection
+  }
+
+  ///
+  /// Change the address that the server connection will utilize.
+  ///
+  pub fn set_address(&mut self, new_address: String) {
+    self.address = new_address;
+  }
+
+  ///
+  /// Change the port that the server connection will utilize.
+  ///
+  pub fn set_port(&mut self, new_port: i32) {
+    self.port = new_port;
+  }
+
+  ///
+  /// Construct the address & port into a parsable socket string.
+  ///
+  pub fn get_socket(&self) -> String {
+    let mut socket = self.address.clone();
+    socket.push(':');
+    socket.push_str(self.port.to_string().as_str());
+
+    socket
+  }
+
+  ///
+  /// Internal initializer procedure automatically run on a new ServerConnection.
+  ///
+  fn initialize(&mut self) {
+    
+
   }
 }
 
