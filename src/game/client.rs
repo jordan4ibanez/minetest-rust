d62942394f61052cb2cea66608bdf52b7bc1155e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-24 11:05:57 -0500

    And then poll

--- src/game/client.rs
+++ src/game/client.rs
@@ -91,6 +91,15 @@ impl<'client> Client<'client> {
   /// This is referred to as on_step in C++ minetest.
   ///
   pub fn on_tick(&mut self, delta: f64) {
+
+    // Poll any incoming network traffic. (non blocking)
+    match &mut self.connection {
+        Some(connection) => {
+          connection.receive();
+        },
+        None => panic!("minetest: tried to listen on non-existent Client connection!"),
+    }
+
     // We want this to throw a runtime panic if we make a logic error.
     // ! Never turn this into a silent bypass via: is_some()
     match &self.lua_engine {
