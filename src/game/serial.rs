2a861b9cf5f1712632bdfe303bd88371216c24e5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-27 05:44:45 -0500

    Document serial.rs

--- src/game/serial.rs
+++ src/game/serial.rs
@@ -1,3 +1,18 @@
+///
+/// Todo:
+///
+/// This is where we shall implement any Serde serialization
+/// and deserialization components purely functionally.
+///
+/// This will clean up code as it's easier to read these two
+/// functions encapsulating a component than it is to read
+/// the (possibly) complex process it _might_ be to utilize serde.
+///
+/// ! This is highly subjective at the moment and will possibly be
+/// ! dropped at any moment due to not having any implementation
+/// ! in this stage of development.
+///
+
 pub fn serialize() {
   println!("serial");
 }
