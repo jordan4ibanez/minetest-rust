018ec9932d17a356aa5926b7b8da2c811f6ae7cf
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-27 05:56:28 -0500

    Make sure they see this

--- src/game/lua_engine.rs
+++ src/game/lua_engine.rs
@@ -139,7 +139,7 @@ impl<'game> LuaEngine<'game> {
   /// Load up a game directly.
   ///
   /// This should _only_ be run on a server LuaEngine.
-  /// ! Never run this function on a Client!
+  /// ! **Never run this function on a Client!**
   ///
   pub fn load_game(&mut self, game_name: String) {
     // We _do not_ want a client to even attempt to load anything.
