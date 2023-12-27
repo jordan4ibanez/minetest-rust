681b09ee1b7c37ed2ec82554354f2d17d858e903
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-27 07:40:02 -0500

    And now we have our first mod

--- games/minetest/mods/main/main.lua
+++ games/minetest/mods/main/main.lua
@@ -1,1 +1,7 @@
-print("minetest/main loaded")
+local minetest = require("api/api")
+
+minetest.register_on_tick(function(delta: number)
+  print("Hello from Luaujit! " .. tostring(delta))
+end)
+
+print("lua: minetest/main loaded")
