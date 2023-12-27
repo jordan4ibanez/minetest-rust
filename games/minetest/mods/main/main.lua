e77a9fa99afda24ca436909cc4f36f2d5285fc01
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-27 07:40:16 -0500

    Make it tidier

--- games/minetest/mods/main/main.lua
+++ games/minetest/mods/main/main.lua
@@ -1,7 +1,7 @@
 local minetest = require("api/api")
 
 minetest.register_on_tick(function(delta: number)
-  print("Hello from Luaujit! " .. tostring(delta))
+  print("Hello from LuauJIT! " .. tostring(delta))
 end)
 
 print("lua: minetest/main loaded")
