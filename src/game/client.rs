ba0285ed68e8beaf0e19ac33a77bb1b279d43075
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-28 14:07:06 -0500

    Drop out pointer and remove lifetime

--- src/game/client.rs
+++ src/game/client.rs
@@ -19,40 +19,23 @@ use super::{lua_engine::LuaEngine, Game};
 /// ? 5.) Handle GameConfig as a component. This should be received from a server
 /// ? 5 - Marked with ? because it's still being thought out at the moment.
 ///
-pub struct Client<'client> {
+pub struct Client {
   client_name: String,
-  connection: Option<ClientConnection<'client>>,
-  lua_engine: Option<LuaEngine<'client>>,
-  game_pointer: Rc<RefCell<Game<'client>>>,
-  client_pointer: Option<Rc<RefCell<Client<'client>>>>,
+  connection: ClientConnection,
+  lua_engine: Option<LuaEngine>,
 }
 
-impl<'client> Client<'client> {
-  pub fn new(
-    game_pointer: Rc<RefCell<Game<'client>>>,
-    client_name: String,
-    address: String,
-    port: i32,
-  ) -> Rc<RefCell<Self>> {
-    let new_client = Rc::new(RefCell::new(Client {
+impl Client {
+  pub fn new(client_name: String, address: String, port: i32) -> Self {
+    let mut new_client = Client {
       client_name,
-      connection: None,
+      connection: ClientConnection::new(address, port),
       lua_engine: None,
-      game_pointer: game_pointer.clone(),
-      client_pointer: None,
-    }));
+    };
 
-    new_client.deref().borrow_mut().client_pointer = Some(new_client.clone());
+    new_client.reset_lua_vm();
 
-    new_client.deref().borrow_mut().connection =
-      Some(ClientConnection::new(new_client.clone(), address, port));
-
-    new_client.deref().borrow_mut().reset_lua_vm();
-
-    println!(
-      "minetest: player name is: {}",
-      &new_client.deref().borrow().client_name
-    );
+    println!("minetest: player name is: {}", &new_client.client_name);
 
     new_client
   }
@@ -83,7 +66,7 @@ impl<'client> Client<'client> {
   /// Creates a new client lua VM.
   ///
   fn create_lua_vm(&mut self) {
-    self.lua_engine = Some(LuaEngine::new(self.game_pointer.clone(), false));
+    self.lua_engine = Some(LuaEngine::new(false));
   }
 
   ///
@@ -105,12 +88,8 @@ impl<'client> Client<'client> {
   ///
   pub fn on_tick(&mut self, delta: f64) {
     // Poll any incoming network traffic. (non blocking)
-    match &mut self.connection {
-      Some(connection) => {
-        connection.receive(delta);
-      }
-      None => panic!("minetest: tried to listen on non-existent Client connection!"),
-    }
+
+    self.connection.receive(delta);
 
     // We want this to throw a runtime panic if we make a logic error.
     // ! Never turn this into a silent bypass via: is_some()
