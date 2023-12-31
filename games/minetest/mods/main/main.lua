ef4e00c56c14344137258662a4419e2f568dc85e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-31 16:49:52 -0500

    The first 3 blocks

--- games/minetest/mods/main/main.lua
+++ games/minetest/mods/main/main.lua
@@ -1,7 +1,28 @@
 local minetest = require("api/api")
 
-minetest.register_on_tick(function(delta: number)
-  print("Hello from LuauJIT! " .. tostring(delta))
-end)
+-- minetest.register_on_tick(function(delta: number)
+--   print("Hello from LuauJIT! " .. tostring(delta))
+-- end)
+
+minetest.register_block({
+  name = "minetest:stone",
+  drawtype = minetest.draw_type.regular,
+  description = "Stone",
+  textures = {"default_stone.png"}
+})
+
+minetest.register_block({
+  name = "minetest:dirt",
+  drawtype = minetest.draw_type.regular,
+  description = "Stone",
+  textures = {"default_dirt.png"}
+})
+
+minetest.register_block({
+  name = "minetest:grass",
+  drawtype = minetest.draw_type.regular,
+  description = "Stone",
+  textures = {"default_stone.png"}
+})
 
 print("lua: minetest/main loaded")
