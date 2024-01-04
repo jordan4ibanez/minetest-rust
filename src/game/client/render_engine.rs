0e3dda527ea3efff641ed33f5bdecc6d5ed9b6f5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-01-04 13:59:08 -0500

    Basic framework on RenderEngine

--- src/game/client/render_engine.rs
+++ src/game/client/render_engine.rs
@@ -1,3 +1,9 @@
+use glam::UVec2;
+use sdl2::video::Window;
+use wgpu_sdl_linker::link_wgpu_to_sdl2;
+
+use crate::file_utilities::read_file_to_string;
+
 ///
 /// The main rendering engine for the game.
 ///
@@ -7,13 +13,169 @@
 /// Utilizes wgpu as the main driving force to render.
 ///
 pub struct RenderEngine {
-  // todo
+  instance: wgpu::Instance,
+  surface: wgpu::Surface,
+  adapter: wgpu::Adapter,
+  device: wgpu::Device,
+  queue: wgpu::Queue,
+  //todo: replace shader with a hashmap of shaders.
+  shader: wgpu::ShaderModule,
+  bind_group_layout: wgpu::BindGroupLayout,
+  bind_group: wgpu::BindGroup,
+  pipeline_layout: wgpu::PipelineLayout,
+  render_pipeline: wgpu::RenderPipeline,
+  surface_format: wgpu::TextureFormat,
+
+  config: wgpu::SurfaceConfiguration,
+  size: UVec2,
 }
 
 impl RenderEngine {
-  pub fn new() -> Self {
+  pub fn new(window: &Window) -> Self {
+    // This is written verbosely so you can read what's going on easier.
+
+    let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+      backends: wgpu::Backends::PRIMARY,
+      flags: wgpu::InstanceFlags::debugging(),
+      dx12_shader_compiler: wgpu::Dx12Compiler::default(),
+      gles_minor_version: wgpu::Gles3MinorVersion::Automatic,
+    });
+
+    let surface = match link_wgpu_to_sdl2(&instance, window) {
+      Ok(new_surface) => new_surface,
+      Err(e) => panic!("{}", e),
+    };
+
+    // We must block the main thread while this completes or things can go crazy.
+    // This is waiting for a future.
+    let adapter_option =
+      pollster::block_on(instance.request_adapter(&wgpu::RequestAdapterOptions {
+        power_preference: wgpu::PowerPreference::HighPerformance,
+        force_fallback_adapter: false,
+        compatible_surface: Some(&surface),
+      }));
+
+    let adapter = match adapter_option {
+      Some(new_adapter) => new_adapter,
+      None => panic!("minetest: no graphics adapter found!"),
+    };
+
+    // We must block the main thread while this completes or things can go crazy.
+    // This is waiting for a future.
+    let (device, queue) = match pollster::block_on(adapter.request_device(
+      &wgpu::DeviceDescriptor {
+        limits: wgpu::Limits::default(),
+        label: Some("gpu"),
+        features: wgpu::Features::default(),
+      },
+      None,
+    )) {
+      Ok(device_and_queue) => device_and_queue,
+      Err(e) => panic!("{}", e),
+    };
+
+    // Load up the default shader source code.
+    let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+      label: Some("default_shader"),
+      source: wgpu::ShaderSource::Wgsl(read_file_to_string("shaders/default.wgsl").into()),
+    });
+
+    // Create bind group components.
+    let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+      entries: &[],
+      label: Some("bind_group_layout"),
+    });
+    let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+      layout: &bind_group_layout,
+      entries: &[],
+      label: Some("bind_group"),
+    });
+
+    // Create the pipeline layout.
+    let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+      bind_group_layouts: &[&bind_group_layout],
+      label: None,
+      push_constant_ranges: &[],
+    });
+
+    // And the pipeline, very important!.
+    let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+      layout: Some(&pipeline_layout),
+      vertex: wgpu::VertexState {
+        buffers: &[],
+        module: &shader,
+        entry_point: "vs_main",
+      },
+      fragment: Some(wgpu::FragmentState {
+        targets: &[Some(wgpu::ColorTargetState {
+          format: wgpu::TextureFormat::Bgra8UnormSrgb,
+          blend: None,
+          write_mask: wgpu::ColorWrites::ALL,
+        })],
+        module: &shader,
+        entry_point: "fs_main",
+      }),
+      primitive: wgpu::PrimitiveState {
+        topology: wgpu::PrimitiveTopology::TriangleList,
+        strip_index_format: None,
+        front_face: wgpu::FrontFace::Ccw,
+        cull_mode: Some(wgpu::Face::Front),
+        unclipped_depth: false,
+        polygon_mode: wgpu::PolygonMode::Fill,
+        conservative: false,
+      },
+      depth_stencil: None,
+      label: None,
+      multisample: wgpu::MultisampleState {
+        count: 1,
+        mask: !0,
+        alpha_to_coverage_enabled: false,
+      },
+      multiview: None,
+    });
+
+    // Surface capabilities.
+    let surface_caps = surface.get_capabilities(&adapter);
+
+    // And the surface format.
+    let surface_format = surface_caps
+      .formats
+      .iter()
+      .copied()
+      // This may not be thorough enough.
+      .find(|f| f.is_srgb())
+      .unwrap_or(surface_caps.formats[0]);
+
+    // Need to get the window size to configure the surface.
+    let (width, height) = window.size();
+
+    let config = wgpu::SurfaceConfiguration {
+      usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+      format: surface_format,
+      width,
+      height,
+      present_mode: wgpu::PresentMode::Fifo,
+      alpha_mode: wgpu::CompositeAlphaMode::Auto,
+      view_formats: Vec::default(),
+    };
+
+    // Then actually configure the surface with the config.
+    surface.configure(&device, &config);
+
     RenderEngine {
-      // todo
+      instance,
+      surface,
+      adapter,
+      device,
+      queue,
+      shader,
+      bind_group_layout,
+      bind_group,
+      pipeline_layout,
+      render_pipeline,
+      surface_format,
+      config,
+      size: UVec2::new(width, height),
     }
   }
 }
