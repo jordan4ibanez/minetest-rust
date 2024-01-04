f17880fb636a0c3fde5778d1e8c56f43ffeab1b8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-01-04 10:18:54 -0500

    Create render_engine.rs

---
+++ src/game/client/render_engine.rs
@@ -0,0 +1,19 @@
+///
+/// The main rendering engine for the game.
+///
+/// This is the meat and potatoes of the game
+/// as far players are concerned.
+///
+/// Utilizes wgpu as the main driving force to render.
+///
+pub struct RenderEngine {
+  // todo
+}
+
+impl RenderEngine {
+  pub fn new() -> Self {
+    RenderEngine {
+      // todo
+    }
+  }
+}
