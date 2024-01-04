90012b3e7e43df4666c97a9734398f7d4a5a2071
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-01-04 00:07:26 -0500

    Create virtual_reality_engine.rs

---
+++ src/game/client/virtual_reality_engine.rs
@@ -0,0 +1,19 @@
+///
+/// I have no hardware to test VR so this is purely speculation.
+///
+/// Consider this the "dream high, set expectations normal" part
+/// of the minetest-rust engine.
+///
+/// I do not know what I would do for this, but here are some ideas.
+///
+pub struct VirtualRealityEngine {
+  // No idea what kind of struct this would be.
+  headset: i32,
+  // No idea what kind of struct this would be.
+  controllers: i32,
+
+  // So this is where the vr headset's "root" position is.
+  center_pos: i32,
+  // And this is the current offset from the "root position".
+  current_offset_pos: i32,
+}
