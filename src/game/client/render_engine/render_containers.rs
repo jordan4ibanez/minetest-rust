9201436c525615fe1c36693a408c4e16199e1ac2
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-01-19 02:48:40 -0500

    Add some basic encapsulation to RenderCall

--- src/game/client/render_engine/render_containers.rs
+++ src/game/client/render_engine/render_containers.rs
@@ -9,3 +9,30 @@ pub struct RenderCall {
   rotation: DVec3,
   scale: DVec3,
 }
+
+impl RenderCall {
+  pub fn new(model_name: &str, translation: DVec3, rotation: DVec3, scale: DVec3) -> Self {
+    RenderCall {
+      model_name: model_name.to_owned(),
+      translation,
+      rotation,
+      scale,
+    }
+  }
+
+  pub fn get_model_name(&self) -> &String {
+    &self.model_name
+  }
+
+  pub fn get_translation(&self) -> &DVec3 {
+    &self.translation
+  }
+
+  pub fn get_rotation(&self) -> &DVec3 {
+    &self.rotation
+  }
+
+  pub fn get_scale(&self) -> &DVec3 {
+    &self.scale
+  }
+}
