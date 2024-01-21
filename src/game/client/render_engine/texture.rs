25e55294cd21bbfdb90636a93a06884f81ca29bd
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-01-21 18:00:25 -0500

    Texture and functions are one

--- src/game/client/render_engine/texture.rs
+++ src/game/client/render_engine/texture.rs
@@ -4,9 +4,15 @@ use crate::file_utilities::read_file_to_byte_vec;
 
 pub struct Texture {}
 
-pub fn blah() {
-  let diffuse_bytes = read_file_to_byte_vec("happy-tree.png");
+impl Texture {
+  pub fn new() -> Self {
+    let diffuse_bytes = read_file_to_byte_vec("happy-tree.png");
 
-  let diffuse_image = image::load_from_memory(diffuse_bytes.as_slice()).unwrap();
-  let diffuse_rgba = diffuse_image.to_rgba8();
+    let diffuse_image = image::load_from_memory(diffuse_bytes.as_slice()).unwrap();
+    let diffuse_rgba = diffuse_image.to_rgba8();
+
+    let dimensions = diffuse_image.dimensions();
+
+    Texture {}
+  }
 }
