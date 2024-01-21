13367aaf4ff58e11fd4943322d56594a0cabd762
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-01-21 17:52:00 -0500

    More base boilerplate experimentation

--- src/game/client/render_engine/texture.rs
+++ src/game/client/render_engine/texture.rs
@@ -1,3 +1,12 @@
-pub struct Texture {
-  
+use image::GenericImageView;
+
+use crate::file_utilities::read_file_to_byte_vec;
+
+pub struct Texture {}
+
+pub fn blah() {
+  let diffuse_bytes = read_file_to_byte_vec("happy-tree.png");
+
+  let diffuse_image = image::load_from_memory(diffuse_bytes.as_slice()).unwrap();
+  let diffuse_rgba = diffuse_image.to_rgba8();
 }
