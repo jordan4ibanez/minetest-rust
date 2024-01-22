50b3c88a3731c53997f279afab0042328ed073d7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-01-21 22:04:01 -0500

    Add placeholder texture coordinates

--- src/game/client/render_engine.rs
+++ src/game/client/render_engine.rs
@@ -245,22 +245,27 @@ impl RenderEngine {
       new_mesh.push_vertex_vec(&mut vec![
         Vertex {
           position: [-0.0868241, 0.49240386, 0.0],
+          texture_coordinates: [0.0, 0.0],
           color: [0.5, 0.0, 0.5],
         }, // A
         Vertex {
           position: [-0.49513406, 0.06958647, 0.0],
+          texture_coordinates: [0.0, 0.0],
           color: [0.5, 0.0, 0.5],
         }, // B
         Vertex {
           position: [-0.21918549, -0.44939706, 0.0],
+          texture_coordinates: [0.0, 0.0],
           color: [0.5, 0.0, 0.5],
         }, // C
         Vertex {
           position: [0.35966998, -0.3473291, 0.0],
+          texture_coordinates: [0.0, 0.0],
           color: [0.5, 0.0, 0.5],
         }, // D
         Vertex {
           position: [0.44147372, 0.2347359, 0.0],
+          texture_coordinates: [0.0, 0.0],
           color: [0.5, 0.0, 0.5],
         }, // E
       ]);
