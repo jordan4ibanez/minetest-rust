5370d80c2df4b52562693b3f50a2c69e918c3169
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-01-22 02:59:51 -0500

    Impl mouse

--- src/game/client/mouse.rs
+++ src/game/client/mouse.rs
@@ -1,3 +1,29 @@
-pub struct Mouse {}
+use glam::UVec2;
 
-impl Mouse {}
+pub struct Mouse {
+  position: UVec2,
+}
+
+impl Mouse {
+  pub fn new() -> Self {
+    Mouse {
+      position: UVec2::new(0, 0),
+    }
+  }
+
+  ///
+  /// Set the Mouse' position.
+  ///
+  /// * This should only be used in window_handler!
+  ///
+  pub fn set_position(&mut self, new_position: UVec2) {
+    self.position = new_position;
+  }
+
+  ///
+  /// Get the Mouse' position.
+  ///
+  pub fn get_position(&self) -> &UVec2 {
+    &self.position
+  }
+}
