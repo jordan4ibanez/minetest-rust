7ec6d4973d82b15db3c2690bfa711961fe68791a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-01-22 00:20:58 -0500

    Fix up & add impl to camera_uniform

--- src/game/client/render_engine/camera/camera_uniform.rs
+++ src/game/client/render_engine/camera/camera_uniform.rs
@@ -1,30 +1,31 @@
 use glam::Mat4;
 
-use crate::game::client::window_handler::WindowHandler;
-
-use super::Camera;
-
 // We need this for Rust to store our data correctly for the shaders
 #[repr(C)]
 // This is so we can store this in a buffer
 #[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
 
-struct CameraUniform {
+pub struct CameraUniform {
   // We can't use cgmath with bytemuck directly, so we'll have
   // to convert the Matrix4 into a 4x4 f32 array
-  view_proj: [[f32; 4]; 4],
+  view_projection: [[f32; 4]; 4],
 }
 
 impl CameraUniform {
-  fn new() -> Self {
+  pub fn new() -> Self {
     Self {
-      view_proj: Mat4::IDENTITY.to_cols_array_2d(),
+      view_projection: Mat4::IDENTITY.to_cols_array_2d(),
     }
   }
 
-  fn update_view_proj(&mut self, camera: &mut Camera, window_handler: &WindowHandler) {
-    self.view_proj = camera
-      .build_view_projection_matrix(window_handler)
-      .to_cols_array_2d();
+  ///
+  /// Updates the raw view projection matrix from the camera.
+  ///
+  pub fn update_view_projection(&mut self, new_matrix: Mat4) {
+    self.view_projection = new_matrix.to_cols_array_2d();
+  }
+
+  pub fn get_view_projection(&self) -> &[[f32; 4]; 4] {
+    &self.view_projection
   }
 }
