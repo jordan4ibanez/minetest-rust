843f76cf89b28d57dde09d184a7f5f25f1d3082e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-01-22 03:04:49 -0500

    More impl for mouse

--- src/game/client/mouse.rs
+++ src/game/client/mouse.rs
@@ -2,22 +2,25 @@ use glam::UVec2;
 
 pub struct MouseController {
   position: UVec2,
+  relative_position: UVec2,
 }
 
 impl MouseController {
   pub fn new() -> Self {
     MouseController {
       position: UVec2::new(0, 0),
+      relative_position: UVec2::new(0, 0),
     }
   }
 
   ///
   /// Set the Mouse' position.
   ///
-  /// * This should only be used in window_handler!
+  /// * This should only be used in WindowHandler!
   ///
-  pub fn set_position(&mut self, new_position: UVec2) {
-    self.position = new_position;
+  pub fn set_position(&mut self, x: u32, y: u32) {
+    self.position.x = x;
+    self.position.y = y;
   }
 
   ///
@@ -25,5 +28,22 @@ impl MouseController {
   ///
   pub fn get_position(&self) -> &UVec2 {
     &self.position
+  }
+
+  ///
+  /// Set the Mouse' relative position.
+  ///
+  /// * This should only be used in WindowHandler!
+  ///
+  pub fn set_relative_position(&mut self, xrel: u32, yrel: u32) {
+    self.relative_position.x = xrel;
+    self.relative_position.y = yrel;
+  }
+
+  ///
+  /// Get the Mouse' relative position.
+  ///
+  pub fn get_relative_position(&self) -> &UVec2 {
+    &self.relative_position
   }
 }
