0853cd06ca59b41844b6e92903f1c0e69b749660
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-01-22 04:55:22 -0500

    Impl ColorUniform

--- src/game/client/render_engine/color_uniform.rs
+++ src/game/client/render_engine/color_uniform.rs
@@ -3,12 +3,14 @@
 // This is so we can store this in a buffer
 #[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
 pub struct ColorUniform {
-  rgb: [f32; 3],
+  rgb: [f32; 4],
 }
 
 impl ColorUniform {
   pub fn new(r: f32, g: f32, b: f32) -> Self {
-    ColorUniform { rgb: [r, g, b] }
+    ColorUniform {
+      rgb: [r, g, b, 1.0],
+    }
   }
 
   pub fn get_r(&self) -> f32 {
@@ -37,5 +39,24 @@ impl ColorUniform {
 
   pub fn get_wgpu_raw_data(&self) -> &[u8] {
     bytemuck::cast_slice(&self.rgb)
+  }
+
+  ///
+  /// Get the wgpu bind group layout to tell wgpu how to use the buffer.
+  ///
+  pub fn get_wgpu_bind_group_layout(device: &wgpu::Device) -> wgpu::BindGroupLayout {
+    device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+      entries: &[wgpu::BindGroupLayoutEntry {
+        binding: 0,
+        visibility: wgpu::ShaderStages::FRAGMENT,
+        ty: wgpu::BindingType::Buffer {
+          ty: wgpu::BufferBindingType::Uniform,
+          has_dynamic_offset: false,
+          min_binding_size: None,
+        },
+        count: None,
+      }],
+      label: Some("color_bind_group_layout"),
+    })
   }
 }
