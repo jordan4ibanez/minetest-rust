d32447256116d64030a41f570c66637e9143ae70
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-01-21 23:06:56 -0500

    Very basic camera

--- src/game/client/render_engine/camera.rs
+++ src/game/client/render_engine/camera.rs
@@ -1,1 +1,27 @@
-pub struct Camera {}
+use glam::DVec3;
+
+use crate::game::client::window_handler::WindowHandler;
+
+pub struct Camera {
+  position: DVec3,
+  target: DVec3,
+  up: DVec3,
+  aspect_ratio: f64,
+  fov_y: f64,
+  z_near: f64,
+  z_far: f64,
+}
+
+impl Camera {
+  pub fn new(position: DVec3, fov_y: f64, window: WindowHandler) -> Self {
+    Camera {
+      position,
+      target: DVec3::new(0.0, 0.0, 0.0),
+      up: glam::DVec3::Y,
+      aspect_ratio: window.get_width() as f64 / window.get_height() as f64,
+      fov_y: 45.0,
+      z_near: 0.1,
+      z_far: 100.0,
+    }
+  }
+}
