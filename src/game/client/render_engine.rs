bc9262135182176173068504a8be734ed096aa3e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-01-22 18:09:11 -0500

    Turn off backface culling

--- src/game/client/render_engine.rs
+++ src/game/client/render_engine.rs
@@ -193,7 +193,7 @@ impl RenderEngine {
         topology: wgpu::PrimitiveTopology::TriangleList,
         strip_index_format: None,
         front_face: wgpu::FrontFace::Ccw,
-        cull_mode: Some(wgpu::Face::Back),
+        cull_mode: None, //Some(wgpu::Face::Back),
         unclipped_depth: false,
         polygon_mode: wgpu::PolygonMode::Fill,
         conservative: false,
