dfe67757460ddee37109ec6314cc5aefd84e0b09
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-01-23 00:07:53 -0500

    Colors

--- src/game/client/render_engine.rs
+++ src/game/client/render_engine.rs
@@ -291,27 +291,27 @@ impl RenderEngine {
         Vertex {
           position: [-0.0868241, 0.49240386, 0.0],
           texture_coordinates: [0.4131759, 0.00759614],
-          color: [1.0, 1.0, 1.0],
+          color: [1.0, 0.0, 0.0],
         }, // A
         Vertex {
           position: [-0.49513406, 0.06958647, 0.0],
           texture_coordinates: [0.0048659444, 0.43041354],
-          color: [1.0, 1.0, 1.0],
+          color: [0.0, 1.0, 0.0],
         }, // B
         Vertex {
           position: [-0.21918549, -0.44939706, 0.0],
           texture_coordinates: [0.28081453, 0.949397],
-          color: [1.0, 1.0, 1.0],
+          color: [0.0, 0.0, 1.0],
         }, // C
         Vertex {
           position: [0.35966998, -0.3473291, 0.0],
           texture_coordinates: [0.85967, 0.84732914],
-          color: [1.0, 1.0, 1.0],
+          color: [1.0, 1.0, 0.0],
         }, // D
         Vertex {
           position: [0.44147372, 0.2347359, 0.0],
           texture_coordinates: [0.9414737, 0.2652641],
-          color: [1.0, 1.0, 1.0],
+          color: [1.0, 0.0, 1.0],
         }, // E
       ]);
 
