db2efe4c9365c1aaf0a0d870e176b6f26e399307
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-01-26 01:08:39 -0500

    ClientConnection is not an option

--- src/game/client/client_connection.rs
+++ src/game/client/client_connection.rs
@@ -25,14 +25,46 @@ pub struct ClientConnection {
 
   lost_connection: bool,
 
-  end_point: Option<Endpoint>,
-  task: Option<NodeTask>,
-  handler: Option<NodeHandler<()>>,
-  event_receiver: Option<EventReceiver<StoredNodeEvent<()>>>,
+  end_point: Endpoint,
+  task: NodeTask,
+  handler: NodeHandler<()>,
+  event_receiver: EventReceiver<StoredNodeEvent<()>>,
 }
 
 impl ClientConnection {
   pub fn new(address: String, port: i32) -> Self {
+    let remote_address = Self::get_socket(&address, port).to_remote_addr().unwrap();
+    let transport_protocol = Transport::Udp;
+
+    // todo: will need to be initialized by the gui component.
+
+    let (handler, listener) = node::split();
+
+    // todo: fixme: this is udp, why are we doing a match here?
+    // todo: If this fails, the user probably doesn't have a network
+    // todo: adapter!
+    let (server_id, local_address) = match handler
+      .network()
+      .connect(transport_protocol, remote_address)
+    {
+      Ok((end_point, local_address)) => {
+        // UDP is connectionless, but it's still good to know it's working.
+        println!(
+          "minetest: established connection to server at id [{}], local address [{}]",
+          end_point, local_address
+        );
+        (end_point, local_address)
+      }
+      Err(e) => panic!("{}", e),
+    };
+
+    let (task, event_receiver) = listener.enqueue();
+    let end_point = server_id;
+
+    // Can possibly be used as a handshake
+    // ! Note: this literally is the handshake right now
+    // self.send_data(self.end_point.unwrap(), "MINETEST_HAND_SHAKE");
+
     let mut new_client_connection = ClientConnection {
       address,
       port,
@@ -47,13 +79,11 @@ impl ClientConnection {
 
       lost_connection: false,
 
-      end_point: None,
-      task: None,
-      handler: None,
-      event_receiver: None,
+      end_point,
+      task,
+      handler,
+      event_receiver,
     };
-
-    new_client_connection.initialize();
 
     new_client_connection
   }
@@ -82,10 +112,10 @@ impl ClientConnection {
   ///
   /// Construct the address & port into a parsable socket string.
   ///
-  pub fn get_socket(&self) -> String {
-    let mut socket = self.address.clone();
+  fn get_socket(address: &str, port: i32) -> String {
+    let mut socket = address.clone().to_owned();
     socket.push(':');
-    socket.push_str(self.port.to_string().as_str());
+    socket.push_str(port.to_string().as_str());
 
     socket
   }
@@ -94,12 +124,7 @@ impl ClientConnection {
   /// Send raw data to the EndPoint (ServerConnection).
   ///
   fn send_data(&self, end_point: Endpoint, data: &str) {
-    self
-      .handler
-      .clone()
-      .unwrap()
-      .network()
-      .send(end_point, data.as_bytes());
+    self.handler.network().send(end_point, data.as_bytes());
   }
 
   ///
@@ -180,7 +205,7 @@ impl ClientConnection {
 
         if self.ping_resend_delta >= 3.0 {
           self.ping_waiting_receive = true;
-          self.send_data(self.end_point.unwrap(), "MINETEST_PING_REQUEST");
+          self.send_data(self.end_point, "MINETEST_PING_REQUEST");
         }
       }
     }
@@ -190,61 +215,16 @@ impl ClientConnection {
   /// Non-blocking event receiver for network events.
   ///
   pub fn receive(&mut self, delta: f64) {
-    match &mut self.event_receiver {
-      Some(event_receiver) => {
-        if let Some(event) = event_receiver.receive_timeout(Duration::new(0, 0)) {
-          match event {
-            StoredNodeEvent::Network(new_event) => self.event_reaction(new_event),
-            // todo: figure out what a signal is!
-            StoredNodeEvent::Signal(_) => todo!(),
-          }
-        }
+    if let Some(event) = self.event_receiver.receive_timeout(Duration::new(0, 0)) {
+      match event {
+        StoredNodeEvent::Network(new_event) => self.event_reaction(new_event),
+        // todo: figure out what a signal is!
+        StoredNodeEvent::Signal(_) => todo!(),
       }
-      None => panic!("minetest: ClientConnection listener does not exist!"),
     }
 
     self.check_handshake(delta);
     self.do_ping_timeout_logic(delta);
-  }
-
-  ///
-  /// Internal initializer procedure automatically run on a new ServerConnection.
-  ///
-  fn initialize(&mut self) {
-    let remote_address = self.get_socket().to_remote_addr().unwrap();
-    let transport_protocol = Transport::Udp;
-
-    // todo: will need to be initialized by the gui component.
-
-    let (handler, listener) = node::split();
-
-    // todo: fixme: this is udp, why are we doing a match here?
-    // todo: If this fails, the user probably doesn't have a network
-    // todo: adapter!
-    let (server_id, local_address) = match handler
-      .network()
-      .connect(transport_protocol, remote_address)
-    {
-      Ok((end_point, local_address)) => {
-        // UDP is connectionless, but it's still good to know it's working.
-        println!(
-          "minetest: established connection to server at id [{}], local address [{}]",
-          end_point, local_address
-        );
-        (end_point, local_address)
-      }
-      Err(e) => panic!("{}", e),
-    };
-
-    let (task, event_receiver) = listener.enqueue();
-    self.end_point = Some(server_id);
-    self.handler = Some(handler);
-    self.task = Some(task);
-    self.event_receiver = Some(event_receiver);
-
-    // Can possibly be used as a handshake
-    // ! Note: this literally is the handshake right now
-    self.send_data(self.end_point.unwrap(), "MINETEST_HAND_SHAKE");
   }
 }
 
@@ -253,7 +233,7 @@ impl Drop for ClientConnection {
     // ClientConnection must stop the handler entity or the Client
     // will not shut down.
     println!("Clientconnection: Shutting down network handler.");
-    NodeHandler::stop(self.handler.as_ref().unwrap());
+    NodeHandler::stop(&self.handler);
     println!("ClientConnection dropped!")
   }
 }
