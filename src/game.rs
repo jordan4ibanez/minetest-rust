c8c44a2ecfdcf5d3e26d1c1c7354e945c9c8afb1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-01-26 02:43:06 -0500

    Print FPS into title every 0.5 seconds

--- src/game.rs
+++ src/game.rs
@@ -81,7 +81,7 @@ impl Game {
     };
 
     let interval = interval(Duration::from_secs_f64(1.0 / loop_helper_goal));
-    let fps_reporter = RateReporter::new(Duration::from_secs(1));
+    let fps_reporter = RateReporter::new(Duration::from_millis(500));
     let delta_reporter = DeltaReporter::new();
 
     //todo: make this happen!
@@ -242,7 +242,12 @@ impl Game {
         true => "FPS",
         false => "TPS",
       };
-      println!("Debug {}: {}", time_measurement, self.current_fps)
+      // println!("Debug {}: {}", time_measurement, self.current_fps)
+      if let Some(client) = &mut self.client {
+        let mut new_title = "minetest ".to_string();
+        new_title.push_str(format!("{:.1}", fps).as_str());
+        client.get_window_handler().set_title(&new_title);
+      }
     }
 
     if self.vsync_mode == 0 || self.is_server {
