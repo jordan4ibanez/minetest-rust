273d4be5fc54ce956e7601091cc7ed2c68623dc1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-01-29 15:21:47 -0500

    I have no idea how to document this

--- src/game/client/render_engine/depth_buffer.rs
+++ src/game/client/render_engine/depth_buffer.rs
@@ -1,3 +1,8 @@
+///
+/// The Depth Buffer does what it says on the tin.
+///
+/// It's the depth buffer. It contains depth.
+///
 pub struct DepthBuffer {
   texture: wgpu::Texture,
   view: wgpu::TextureView,
@@ -9,7 +14,6 @@ impl DepthBuffer {
 
   pub fn new(device: &wgpu::Device, config: &wgpu::SurfaceConfiguration, label: &str) -> Self {
     let size = wgpu::Extent3d {
-      // 2.
       width: config.width,
       height: config.height,
       depth_or_array_layers: 1,
@@ -21,22 +25,20 @@ impl DepthBuffer {
       sample_count: 1,
       dimension: wgpu::TextureDimension::D2,
       format: Self::DEPTH_FORMAT,
-      usage: wgpu::TextureUsages::RENDER_ATTACHMENT // 3.
-          | wgpu::TextureUsages::TEXTURE_BINDING,
+      usage: wgpu::TextureUsages::RENDER_ATTACHMENT | wgpu::TextureUsages::TEXTURE_BINDING,
       view_formats: &[],
     };
     let texture = device.create_texture(&desc);
 
     let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
     let sampler = device.create_sampler(&wgpu::SamplerDescriptor {
-      // 4.
       address_mode_u: wgpu::AddressMode::ClampToEdge,
       address_mode_v: wgpu::AddressMode::ClampToEdge,
       address_mode_w: wgpu::AddressMode::ClampToEdge,
       mag_filter: wgpu::FilterMode::Linear,
       min_filter: wgpu::FilterMode::Linear,
       mipmap_filter: wgpu::FilterMode::Nearest,
-      compare: Some(wgpu::CompareFunction::LessEqual), // 5.
+      compare: Some(wgpu::CompareFunction::LessEqual),
       lod_min_clamp: 0.0,
       lod_max_clamp: 100.0,
       ..Default::default()
