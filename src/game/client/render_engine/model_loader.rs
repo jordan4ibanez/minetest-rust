51d08af0f53b71ab2bffb2d1a72af59f11d6a561
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-02-01 02:20:31 -0500

    I'm glad I thought this out

--- src/game/client/render_engine/model_loader.rs
+++ src/game/client/render_engine/model_loader.rs
@@ -1,5 +1,17 @@
+use crate::file_utilities::file_extension_from_path;
+
 mod obj_loader;
 
-pub fn load_model(directory: &str) {
+///
+/// Load a model up without having to worry about file extensions.
+///
+/// If someone wants to add a new file format loader integration
+/// for the render engine, they just bolt in a new extension/function here.
+///
+pub fn load_model(path: &str) {
   println!("Hello I am loading hooray!");
+
+  let extension = file_extension_from_path(path);
+
+  println!("the extension is {}", extension);
 }
