df6886d491e32f2fb643267dc5a665f7d240c6df
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-02-03 01:03:40 -0500

    Add some more stuff to Model

--- src/game/client/render_engine/model.rs
+++ src/game/client/render_engine/model.rs
@@ -1,6 +1,13 @@
-use super::{mesh::Mesh, texture::Texture};
+use super::mesh::Mesh;
 
+///
+/// ! Fixme: this should be immutable, don't change models during runtime.
+/// ! use encapsulation to stop this from getting changed with a lockout.
+///
 pub struct Model {
   pub meshes: Vec<Mesh>,
-  pub materials: Vec<Texture>,
+  pub number_of_texture_buffers: u32,
+  // todo: use this to lockout the model from changing and be readonly.
+  // todo: You should have to completely regenerate a new model.
+  lock: bool,
 }
