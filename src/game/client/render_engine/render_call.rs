9196cc3487800000f0573de0551d6e11b6f758df
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-02-03 01:25:54 -0500

    Duct tape on a new render struct

--- src/game/client/render_engine/render_call.rs
+++ src/game/client/render_engine/render_call.rs
@@ -68,3 +68,69 @@ impl RenderCall {
     &self.scale
   }
 }
+
+///
+/// Unbatched render call struct.
+///
+/// If you want to batch it, see BatchRenderCall.
+///
+///
+pub struct ModelRenderCall {
+  mesh_name: String,
+  texture_names: Vec<String>,
+  translation: Vec3A,
+  rotation: Vec3A,
+  scale: Vec3A,
+}
+impl ModelRenderCall {
+  pub fn new(
+    mesh_name: &str,
+    texture_name: &str,
+    translation: Vec3A,
+    rotation: Vec3A,
+    scale: Vec3A,
+  ) -> Self {
+    RenderCall {
+      mesh_name: mesh_name.to_owned(),
+      texture_name: texture_name.to_owned(),
+      translation,
+      rotation,
+      scale,
+    }
+  }
+
+  ///
+  /// Get the RenderCall's mesh name.
+  ///
+  pub fn get_mesh_names(&self) -> &Vec<String> {
+    &self.mesh_name
+  }
+
+  ///
+  /// Get the texture that will be used for this RenderCall.
+  ///
+  pub fn get_texture_name(&self) -> &String {
+    &self.texture_name
+  }
+
+  ///
+  /// Get the translation of the RenderCall.
+  ///
+  pub fn get_translation(&self) -> &Vec3A {
+    &self.translation
+  }
+
+  ///
+  /// Get the rotation of the RenderCall.
+  ///
+  pub fn get_rotation(&self) -> &Vec3A {
+    &self.rotation
+  }
+
+  ///
+  /// Get the scale of the RenderCall.
+  ///
+  pub fn get_scale(&self) -> &Vec3A {
+    &self.scale
+  }
+}
