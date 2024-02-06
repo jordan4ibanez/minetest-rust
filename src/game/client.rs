c38741a19eaf3a41acb3285fff92c4214ffaa168
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-02-06 02:51:41 -0500

    Add some true insanity

--- src/game/client.rs
+++ src/game/client.rs
@@ -301,15 +301,15 @@ impl Client {
     let mut i = 0.0;
     for x in 0..TESTING_LIMIT {
       for z in 0..TESTING_LIMIT {
-        color += 0.001375;
-        if color >= 1.0 {
+        color += 0.13;
+        if color > 1.0 {
           color = 0.0;
         }
         instancing_tf.push(InstanceMatrixRGBA::new(
           vec3a(x as f32, 0.0, z as f32),
           vec3a(0.0, self.spin_test as f32 + i, 0.0),
           vec3a(1.0, 1.0, 1.0),
-          vec4(1.0, color as f32, 1.0, 1.0),
+          vec4(color as f32, color as f32, 1.0, 1.0),
         ));
         i += 0.05;
       }
