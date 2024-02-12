f64f8b1ab13e9cca9a1be66b4862ba5039c49d5c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-02-11 22:30:59 -0500

    Let's see how this goes

--- src/game/client/render_engine/model_loader/gltf_loader.rs
+++ src/game/client/render_engine/model_loader/gltf_loader.rs
@@ -1,4 +1,9 @@
-use crate::game::client::render_engine::model::Model;
+use gltf::Gltf;
+
+use crate::{
+  file_utilities::{file_name_from_path, read_path_to_buf_read},
+  game::client::render_engine::model::Model,
+};
 
 ///
 /// The GLTF file loader.
@@ -8,5 +13,28 @@ use crate::game::client::render_engine::model::Model;
 pub struct GLTFLoader {}
 
 impl GLTFLoader {
-  pub fn load(path: &str, device: &wgpu::Device, queue: &wgpu::Queue) -> Model {}
+  pub fn load(path: &str, device: &wgpu::Device, queue: &wgpu::Queue) /*-> Model*/
+  {
+    // The file name. This will be used later.
+    let file_name = match file_name_from_path(path) {
+      Ok(file_name) => file_name,
+      Err(e) => panic!("GLTFLoader: {}", e),
+    };
+
+    // The buffer we're going to read the model into.
+    let mut model_reader = match read_path_to_buf_read(path) {
+      Ok(model_reader) => model_reader,
+      Err(e) => panic!("GLTFLoader: {}", e),
+    };
+
+    // Now if there was an issue, stop everything.
+    // !TODO: Maybe in the future we can just return out a result from this.
+    // ! But this is currently being written from scratch at the time of this comment.
+    let gltf = match Gltf::from_reader(model_reader) {
+      Ok(gltf) => gltf,
+      Err(e) => panic!("GLTFLoader: {}", e),
+    };
+
+    println!("{:#?}", gltf);
+  }
 }
