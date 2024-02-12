6a5f383c6b69eaf95af9f11086429ae369ff4533
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-02-12 06:40:02 -0500

    Now we move into usability

--- src/game/client/render_engine/model_loader/gltf_loader.rs
+++ src/game/client/render_engine/model_loader/gltf_loader.rs
@@ -1,7 +1,5 @@
-use crate::{
-  file_utilities::{file_name_from_path, read_path_to_buf_read},
-  game::client::render_engine::mesh::Mesh,
-};
+use crate::file_utilities::file_name_from_path;
+
 ///
 /// The GLTF file loader.
 ///
@@ -12,9 +10,19 @@ pub struct GLTFLoader {}
 impl GLTFLoader {
   pub fn load(path: &str, device: &wgpu::Device, queue: &wgpu::Queue) /*-> Model*/
   {
+    // The file name. This will be used later.
+    let file_name = match file_name_from_path(path) {
+      Ok(file_name) => file_name,
+      Err(e) => panic!("GLTFLoader: {}", e),
+    };
+
     let scenes = match easy_gltf::load(path) {
       Ok(data) => data,
       Err(e) => panic!("GLTFLoader: {}", e),
     };
+
+    if scenes.is_empty() {
+      panic!("GLTFLoader: {} is a blank model!", file_name)
+    }
   }
 }
